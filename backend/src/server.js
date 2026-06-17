9b10ef6d5d59c8e558347bc031d9deb199bf7e15
Saptarshi Manna <mannasaptarshi1@gmail.com>
2026-06-17 11:10:20 +0530

    changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { ENV } from "./lib/env.js"
 
+const PORT = ENV.PORT || 3000;
+
 const app = express()
 
 app.get("/", (req, res) => {
   res.status(200).json({message: "success from backend"})
 })
-
-const PORT = ENV.PORT;
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:/${PORT}`));
