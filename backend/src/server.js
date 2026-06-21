50c241dcb7d7ae0c6f41d690744a7533692641e3
Saptarshi Manna <mannasaptarshi1@gmail.com>
2026-06-21 20:13:18 +0530

    session routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import { inngest, functions } from "./lib/inngest.js";
 import { clerkMiddleware } from '@clerk/express'
 import { protectRoute } from "./middleware/protectRoute.js";
 import chatRoutes from "./routes/chatRoutes.js"
+import sessionRoutes from "./routes/sessionRoutes.js"
 
 const PORT = ENV.PORT || 3000;
 
@@ -26,7 +27,8 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); //this adds auth field to request object: req.auth(); 
 
 app.use("/api/inngest", serve({client: inngest, functions}));
-app.use("/api/chat", chatRoutes)
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "success from backend" });
