3205085557323a9363ad013ed87a45e173de0541
Saptarshi Manna <mannasaptarshi1@gmail.com>
2026-06-21 20:27:25 +0530

    changes added to the session controller to make it more robust

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -106,8 +106,16 @@ export async function joinSession(req, res) {
 
     if (!session) return res.status(404).json({ message: "Session not found" });
 
+    if (session.status !== "active")
+      return res.status(400).json({ message: "Session is already completed" });
+
+    if (session.host.toString() === userId.toString())
+      return res
+        .status(400)
+        .json({ message: "Host cannot be the participant" });
+
     if (session.participant)
-      return res.status(404).json({ message: "Session is full" });
+      return res.status(409).json({ message: "Session is full" });
     session.participant = userId;
     await session.save();
 
@@ -136,14 +144,14 @@ export async function endSession(req, res) {
     if (session.status === "completed")
       return res.status(400).json({ message: "Session already ended" });
 
-    session.status = "completed";
-    await session.save();
-
     const call = streamClient.video.call("default", session.callId);
     await call.delete({ hard: true });
 
     const channel = chatClient.channel("messaging", session.callId);
     await channel.delete();
+
+    session.status = "completed";
+    await session.save();
 
     res.status(200).json({ session, message: "Session ended succefully" });
   } catch (error) {
